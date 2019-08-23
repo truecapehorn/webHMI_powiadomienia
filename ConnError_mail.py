8d8edc1b77943c8e604b6c885d7b4a0665205952
truecapehorn <truecapehorn@gmail.com>
2019-08-23 22:17:59 +0200

    dodanie tabeli do wiadomosci

--- ConnError_mail.py
+++ ConnError_mail.py
@@ -10,6 +10,7 @@ from API_webHMI import *
 from defs import *
 from head import headers, device_adress,filepath
 import pandas as pd
+import numpy as np
 
 
 # In[2]:
@@ -83,6 +84,8 @@ header1['X-WH-CONNS']='262' # 262 to numer polaczenia w webHMI
 val=reg_val(device_adress,header1)
 
 
+# ## Odczytanie tablicy z scantime
+
 # In[9]:
 
 
@@ -90,9 +93,39 @@ import ast
 scan_time=[ast.literal_eval(x) for x in val['4547']['v'].split(";")]
 
 
-# ## Nadpisanie pliku wiadomosci
+# ## Stworzenie tablicy
 
 # In[10]:
+
+
+scan_dict={}
+for n,i in enumerate(scan_time):
+    scan_dict[n]={'ids':i[0],
+                  'title':connections.loc[str(i[0]),'title'],
+                  'category':connections.loc[str(i[0]),'category'],
+                  'timeout':connections.loc[str(i[0]),'timeout'],
+                  'disabled':connections.loc[str(i[0]),'disabled'],
+                  'scan_time':int(i[1])}
+
+
+# In[11]:
+
+
+scan_frame=pd.DataFrame(scan_dict,).T.apply(f)
+frame=scan_frame.sort_values('scan_time',ascending=False)
+
+
+# In[12]:
+
+
+pivot_sum=frame.pivot_table(index='category',values='scan_time',aggfunc=[np.mean,np.sum])
+# df=df.swaplevel(0,1,axis=1)
+pivot_sum=pivot_sum.swaplevel(0,1,axis=1).sort_values(('scan_time','sum'),ascending=False)
+
+
+# ## Nadpisanie pliku wiadomosci
+
+# In[13]:
 
 
 connectio_problem=val['4546']['v'].split(",") # wiadomosc do zapisania
@@ -100,23 +133,23 @@ with open('mail_message.txt', 'w')  as w_writer:
     if len(connectio_problem)>1:
         for i in connectio_problem:
             title = connections.loc[i,'title']
-            print("Błąd połączenia nr {} - {}\n".format(i,title ))
-            mesages="Błąd połączenia nr {} - {}\n".format(i,title )
+#             print("Błąd połączenia nr {} - {}\n".format(i,title ))
+            mesages="<p>Błąd połączenia nr {} - {}</p>\n".format(i,title )
             w_writer.write(mesages)
-    else: w_writer.write('Brak problemow z połączeniami. :)\n\n')
-with open('mail_message.txt', 'a')  as a_writer:
-    for i in scan_time:
-        title = connections.loc[str(i[0]),'title']
-#         print("Scan time dla {} wynosi {}".format(title,i[1]))
-        mesages2 = "Scan time dla {} wynosi {}\n".format(title,i[1])
-        a_writer.write(mesages2)
+    else: w_writer.write('<h3>Brak problemow z połączeniami. :)</h3>\n\n')
+# with open('mail_message.txt', 'a')  as a_writer:
+#     for i in scan_time:
+#         title = connections.loc[str(i[0]),'title']
+# #         print("Scan time dla {} wynosi {}".format(title,i[1]))
+#         mesages2 = "Scan time dla {} wynosi {}\n".format(title,i[1])
+#         a_writer.write(mesages2)
  
         
 
 
 # ## Wyslanie maila
 
-# In[11]:
+# In[17]:
 
 
 from envelopes import Envelope, GMailSMTP
@@ -124,35 +157,40 @@ import glob
 
 def send_emial(content):
 
-    addr=['tito02@o2.pl', 'norbert.jablonski@elam.pl','michal.marchelewski@elam.pl']
-    email= u"WebHMI ma problemy z komunikacją dla następujących urządzeń:\n{}".format(str(content))
+    addr=['tito02@o2.pl','norbert.jablonski@elam.pl','michal.marchelewski@elam.pl']
+    email= u"<h2>WebHMI ma problemy z komunikacją dla następujących urządzeń:</h2>\n{}\n{}".format(str(content),pivot_sum.to_html())
+#     email2=pivot_sum.to_html()
     for i in range(len(addr)):
         print('Wysłanie wiadomosci do {}'.format(addr[i]))
         envelope = Envelope(
             from_addr=( u'WebHMI - Raport'),
             to_addr=(addr[i]),
             subject=u'Raport o komunikacji {} dla {}'.format(i+1,addr[i]),
-            text_body=email)
+            html_body=email,
+#             text_body=email
+        )
         #envelope.add_attachment(str(atachment))
 
     # Send the envelope using an ad-hoc connection...
-        envelope.send('smtp.googlemail.com', login='truecapehorn@gmail.com',password='Ptjczinp249', tls=True)
+        send_msg=envelope.send('smtp.googlemail.com', login='truecapehorn@gmail.com',password='Ptjczinp249', tls=True,)
         print('email do adresata. {} wysłany.'.format(addr[i]))
+    print(email)    
+    return send_msg
 
 
-# In[12]:
+# In[15]:
 
 
 if len(connectio_problem)>0:
     with open('mail_message.txt', 'r') as content_file:
         content = content_file.read()
-        send_emial(content)
+        send_msg=send_emial(content)
 
 
-# In[ ]:
+# In[16]:
 
 
-
+send_msg
 
 
 # In[ ]:
